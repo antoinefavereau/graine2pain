7fa07b2d04aa7ee03041e7682835c308abb945fd
Antoine Favereau <antoinefavereau45@gmail.com>
2025-08-15 11:02:46 +0200

    refactor: update font variable usage for Julius Sans One in layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
+import { Julius_Sans_One } from "next/font/google";
 import "./globals.css";
 import "material-symbols";
+
+const juliusSansOne = Julius_Sans_One({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-julius",
+});
 
 export const metadata: Metadata = {
   title: "Moreau Athéna",
@@ -14,7 +21,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
-      <body className="antialiased bg-black text-white">{children}</body>
+      <body
+        className={`antialiased bg-black text-white ${juliusSansOne.variable}`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
