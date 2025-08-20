eff99d5c3f43b1d0b339c6a9a56d7755bb2d7542
Antoine Favereau <antoinefavereau45@gmail.com>
2025-08-20 22:11:32 +0200

    Add projects fixed button

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -8,6 +8,14 @@ export default function Home() {
         <Header />
         <Hero />
       </div>
+      <a
+        href="/projets"
+        className="group fixed bottom-12 right-12 p-[2px] rounded-full bg-linear-to-b/srgb from-primary to-secondary"
+      >
+        <div className="p-4 rounded-full bg-dark font-bold group-hover:bg-dark-hover">
+          VOIR PROJETS
+        </div>
+      </a>
     </>
   );
 }
