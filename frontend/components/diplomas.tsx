f9e23fed6f15a7dc9ed547189f4f377aa306435b
Antoine Favereau <antoinefavereau45@gmail.com>
2025-12-12 20:14:43 +0100

    Add SVG icons for Pr, Ps, Vsc, and Xd software with gradient fills and paths

--- frontend/components/diplomas.tsx
+++ frontend/components/diplomas.tsx
@@ -27,47 +27,47 @@ const diplomas = [
 
 const softwares = [
   {
-    img: "/software/illustrator.svg",
+    img: "/softwares/Ai.svg",
     title: "Illustrator",
     mastery: 75,
   },
   {
-    img: "/software/photoshop.svg",
+    img: "/softwares/Ps.svg",
     title: "Photoshop",
     mastery: 75,
   },
   {
-    img: "/software/after-effects.svg",
+    img: "/softwares/Ae.svg",
     title: "After Effects",
     mastery: 25,
   },
   {
-    img: "/software/adobe-xd.svg",
+    img: "/softwares/Xd.svg",
     title: "Adobe XD",
     mastery: 45,
   },
   {
-    img: "/software/in-design.svg",
+    img: "/softwares/Id.svg",
     title: "In Design",
     mastery: 50,
   },
   {
-    img: "/software/premiere-pro.svg",
+    img: "/softwares/Pr.svg",
     title: "Première Pro",
     mastery: 25,
   },
   {
-    img: "/software/visual-studio-code.svg",
+    img: "/softwares/Vsc.svg",
     title: "Visual Studio Code",
     mastery: 50,
   },
   {
-    img: "/software/blender.svg",
+    img: "/softwares/Bl.svg",
     title: "Blender",
     mastery: 25,
   },
   {
-    img: "/software/figma.svg",
+    img: "/softwares/Fi.svg",
     title: "Figma",
     mastery: 95,
   },
@@ -86,8 +86,36 @@ const languages = [
   },
 ];
 
+const hobbies = [
+  {
+    img: "/hobbies/harpe.jpg",
+    title: "Instrument",
+    description: "Harpe (14 ans)",
+  },
+  {
+    img: "/hobbies/batterie.jpg",
+    title: "Instrument",
+    description: "Percussion/batterie (3 ans)",
+  },
+  {
+    img: "/hobbies/",
+    title: "Canni-rando",
+    description: "",
+  },
+  {
+    img: "/hobbies/renfo.jpg",
+    title: "Renforcement musculaire",
+    description: "Pendant 5 ans",
+  },
+  {
+    img: "/hobbies/",
+    title: "Autres instruments sans cours",
+    description: "Flûte à bec, flute traversière, piano, guitare",
+  },
+];
+
 export default function Diplomas() {
-  const swiperParams: SwiperOptions = {
+  const titleSwiperParams: SwiperOptions = {
     modules: [Autoplay],
     autoplay: {
       delay: 2500,
@@ -98,8 +126,14 @@ export default function Diplomas() {
     spaceBetween: 10,
   };
 
+  const hobbySwiperParams: SwiperOptions = {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+  };
+
   return (
-    <section className="relative flex flex-col items-center gap-8 p-8">
+    <section className="relative flex flex-col items-center gap-12 py-16 px-8">
       <h2 className="text-7xl font-heading font-bold">
         DIPL<span className="sr-only">Ô</span>
         <div
@@ -107,7 +141,7 @@ export default function Diplomas() {
           aria-hidden="true"
         >
           <div className="absolute inset-[0.24cap_0.28cap] bg-black rounded-[0.4cap]">
-            <Swiper {...swiperParams} className="h-full">
+            <Swiper {...titleSwiperParams} className="h-full">
               {diplomas.map((diploma, index) => (
                 <SwiperSlide
                   key={index}
@@ -128,18 +162,28 @@ export default function Diplomas() {
         </div>
         <span className="relative">MES</span>
       </h2>
-      <div className="grid grid-cols-2 gap-4">
-        <div className="grid gap-8 bg-dark rounded-xl p-4">
+      <div className="grid grid-cols-2 gap-4 max-w-4xl">
+        <div className="grid gap-8 bg-dark rounded-xl p-4 pb-8">
           <h3 className="text-xl font-heading font-light">LOGICIELS</h3>
-          <div className="grid grid-cols-3">
+          <div className="grid grid-cols-3 gap-x-6 gap-y-4 px-8 text-xs">
             {softwares.map((software, index) => (
-              <div key={index} className="flex flex-col items-center">
-                <p>{software.title}</p>
+              <div key={index} className="grid gap-2">
+                <Image
+                  src={software.img}
+                  alt={software.title}
+                  width={56}
+                  height={56}
+                  className="w-full h-auto"
+                />
+                <div className="flex flex-col items-center">
+                  <h4 className="font-bold">{software.title}</h4>
+                  <p>{software.mastery}%</p>
+                </div>
               </div>
             ))}
           </div>
         </div>
-        <div className="grid grid-rows-[auto_1fr]">
+        <div className="grid grid-rows-[auto_1fr] gap-4">
           <div className="grid gap-8 bg-dark rounded-xl p-4">
             <h3 className="text-xl font-heading font-light">LANGUES</h3>
             <div className="grid grid-cols-2 gap-4">
@@ -164,7 +208,26 @@ export default function Diplomas() {
             <h3 className="text-xl font-heading font-light">
               ACTIVITÉS EXTRASCOLAIRES
             </h3>
-            <div className=""></div>
+            <div className="grid">
+              <Swiper {...hobbySwiperParams}>
+                {hobbies.map((hobby, index) => (
+                  <SwiperSlide key={index}>
+                    <div className="relative">
+                      <Image
+                        src={hobby.img}
+                        alt={hobby.title}
+                        width={200}
+                        height={200}
+                      />
+                      <div className="absolute left-0 right-0 bottom-0 bg-dark bg-opacity-70 text-white p-2 blur-md">
+                        <h4 className="text-xs">{hobby.title}</h4>
+                        <p className="text-xs">{hobby.description}</p>
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
           </div>
         </div>
       </div>
