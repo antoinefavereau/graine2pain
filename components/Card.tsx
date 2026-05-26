1e7de6bcc68348048d7abf421b71548209ed24aa
antoinefavereau <antoinefavereau45@gmail.com>
2026-05-26 19:22:02 +0200

    fix card

--- components/Card.tsx
+++ components/Card.tsx
@@ -13,26 +13,27 @@ const Card = forwardRef<HTMLDivElement, CardProps>(function Card(
   ref,
 ) {
   return (
-    <RefractiveDiv
-      ref={ref}
-      {...props}
-      className={twMerge(
-        "rounded-[20px]",
-        // "relative isolate bg-grey-darkest/30 backdrop-blur-sm",
-        // "before:absolute before:inset-0 before:-z-10 before:rounded-[20px] before:p-px before:content-['']",
-        // "before:bg-[linear-gradient(to_bottom_right,var(--color-grey-base),var(--color-grey-dark),var(--color-grey-base))]",
-        // "before:[mask:linear-gradient(#000_0_0)_content-box,linear-gradient(#000_0_0)]",
-        // "before:mask-exclude!",
-        className,
-      )}
-      refraction={{
-        radius: 40,
-        blur: 6,
-        bezelWidth: 32,
-      }}
-    >
-      {children}
-    </RefractiveDiv>
+    <div ref={ref}>
+      <RefractiveDiv
+        {...props}
+        className={twMerge(
+          "rounded-[20px]",
+          // "relative isolate bg-grey-darkest/30 backdrop-blur-sm",
+          // "before:absolute before:inset-0 before:-z-10 before:rounded-[20px] before:p-px before:content-['']",
+          // "before:bg-[linear-gradient(to_bottom_right,var(--color-grey-base),var(--color-grey-dark),var(--color-grey-base))]",
+          // "before:[mask:linear-gradient(#000_0_0)_content-box,linear-gradient(#000_0_0)]",
+          // "before:mask-exclude!",
+          className,
+        )}
+        refraction={{
+          radius: 40,
+          blur: 6,
+          bezelWidth: 32,
+        }}
+      >
+        {children}
+      </RefractiveDiv>
+    </div>
   );
 });
 
